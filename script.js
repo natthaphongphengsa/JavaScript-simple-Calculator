8d9dc71ee8aa7a1c037fa80402fed3d82bfcb24e
Natthaphong Phengsa <Natthaphong.phengsa@outlook.com>
2023-01-26 19:38:33 +0100

    Style and fix functions

--- script.js
+++ script.js
@@ -1,75 +1,73 @@
-// let articleElement = document.querySelector("article");
-
-// let varor = [
-//     "Nike skor",
-//     "Convers skor",
-//     "Addidas skor",
-//     "Levis jeans"
-// ]
-// for (let i = 0; i < varor.length; i++) {
-//     const vara = varor[i];
-//     const pElement = document.createElement('p');
-//     pElement.textContent = vara;
-//     articleElement.appendChild(pElement);
-    
-// }
-
-// function DisplayStatus(){
-//     debugger
-//     const numberinput = document.getElementById('inputElement').value;  
-
-//     const number = document.getElementById('status').value += numberinput + document.getElementById('operator').value;
-//     document.getElementById('status').innerHTML = number; 
-
-// }
-// function Result(){
-//     debugger
-//     let x = document.getElementById('status').value;
-//     let result = eval(x);
-//     const RESULT = document.getElementById('status').value += document.getElementById('operator').value + result;  
- 
-// }
-
 //function that display value 
-function dis(val){
-    debugger
-    const clrtext = "";
-    document.getElementById("result").value += clrtext; 
-    const result = document.getElementById("result").value += val;
-
-    const empty = ""; 
-    document.getElementById("Update").value += result;
-    document.getElementById("result").value = empty;    
+function Input(val){        
+    if(val == "+" || val == "-" || val == "/" || val == "*"){
+        var previus = document.getElementById("result");
+        solve(previus);
+    }  
+    else{
+        const inputValue = document.getElementById("current").value += val.value;
+        document.getElementById("current").value = inputValue; 
+    }
 } 
 
 // function that clear the digit
-function clr(val){
-    debugger
-    const empty = ""; 
-    if(val == "AC"){        
-        document.getElementById("Update").value = empty;
-        document.getElementById("resultonly").value = empty;
+function Clear(val){
+    if(val.value == "AC"){        
+        document.getElementById("current").value = "";  
+        // document.getElementById("result").value = "";
     }
-    else if(val == "C")
-    {
-        document.getElementById("Update").value = empty;      
+    else if(val.value == "C")
+    {        
+        let digit = document.getElementById("current");
+        let result = digit.value.slice(0,-1);
+        digit.value = result;
     }
-}
-function deletetext(){
-    debugger
-    const clrtext = "";
-    document.getElementById("result").value = clrtext;    
 }
           
-//function that evaluates the digit and return result 
-function solve() { 
+function previus(val){
     debugger
-    const empty = ""; 
-    let e = document.getElementById("result").value;
-    document.getElementById("Update").value += e; 
-    let x = document.getElementById("Update").value;
-    document.getElementById("result").value = empty;
-    let y = eval(x) 
-    document.getElementById("Update").value += document.getElementById('qeuals').value + y;
-    document.getElementById('resultonly').value = y;
+    let beforeEqual = val.split('=');
+    solve(beforeEqual[0]);
 }
+//function that evaluates the digit and return result 
+function solve(val) { 
+    let previusresults = document.getElementById('previusresults');
+    let currentDigit = document.getElementById('current').value;
+    let optionElement = document.createElement('option');
+    const historiesoptions = [];
+
+    if(currentDigit != "0"){
+        if(val.value == "="){            
+            var result = eval(currentDigit);       
+            document.getElementById("current").value = result;
+    
+            optionElement.value = `${currentDigit}=${result}`;
+            optionElement.textContent = `${currentDigit}=${result}`;
+    
+            if(previusresults.children.length == 0){
+                previusresults.append(optionElement); 
+            }
+            for (let index = 0; index < previusresults.children.length; index++) {
+                historiesoptions.push(previusresults.children[index].innerHTML)           
+            }
+    
+            if(historiesoptions.includes(optionElement.innerHTML) != true){
+                previusresults.append(optionElement);  
+            }
+        }
+        else{
+            debugger  
+            document.getElementById("current").value = val;
+        }
+    } 
+    previusresults.removeAttribute("style");
+}
+
+ function clearHistory(){
+    let user_input = confirm("Your result history will be deleted");
+    if(user_input == true){
+        let historyElement = document.getElementById('previusresults');
+        historyElement.innerHTML = "";
+        historyElement.setAttribute("style","display:none");
+    }
+ }
